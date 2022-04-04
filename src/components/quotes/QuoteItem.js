90647f6a12ecbf91b4c37b661267059bc4b322eb
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-04 20:35:39 +0800

    feat: outputtimg data on the detail page

--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -1,6 +1,7 @@
-import classes from './QuoteItem.module.css';
+import classes from './QuoteItem.module.css'
+import { Link } from 'react-router-dom'
 
-const QuoteItem = (props) => {
+const QuoteItem = props => {
   return (
     <li className={classes.item}>
       <figure>
@@ -9,11 +10,11 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className='btn'>
+      <Link className="btn" to={`/quotes/${props.id}`}>
         View Fullscreen
-      </a>
+      </Link>
     </li>
-  );
-};
+  )
+}
 
-export default QuoteItem;
+export default QuoteItem
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,20 @@
 import { useParams, Route } from 'react-router-dom'
 import { Fragment } from 'react'
+import HighlightQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments'
-
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Ashley', text: 'Hello world!' },
+  { id: 'q2', author: 'Ashley', text: 'hi!' }
+]
 const QuoteDetail = () => {
   const params = useParams()
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
   return (
     <Fragment>
-      <h1> QuoteDetail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
