71c658b5fe3f542b3e5ef9114f4a507a6dfd8a58
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-04 18:50:19 +0800

    feat: define routes and pages

---
+++ src/pages/NewQuote.js
@@ -0,0 +1,4 @@
+const NewQuote = () => {
+  return <h1>New Quote</h1>
+}
+export default NewQuote
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,4 @@
+const QuoteDetail = () => {
+  return <h1> QuoteDetail</h1>
+}
+export default QuoteDetail
