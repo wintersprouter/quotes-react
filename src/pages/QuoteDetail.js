728a02140123334b18015f62fb30d37a4e24b7f3
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-04 19:06:09 +0800

    feat: add nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,6 @@
 import { useParams, Route } from 'react-router-dom'
 import { Fragment } from 'react'
+import Comments from '../components/comments/Comments'
 
 const QuoteDetail = () => {
   const params = useParams()
@@ -7,6 +8,9 @@ const QuoteDetail = () => {
     <Fragment>
       <h1> QuoteDetail</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   )
 }
