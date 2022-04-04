a91b06cad7f67f6b5b14f3cc5a881390b8a02f64
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-04 19:01:08 +0800

    feat: redirecting and extracting params

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,13 @@
+import { useParams, Route } from 'react-router-dom'
+import { Fragment } from 'react'
+
 const QuoteDetail = () => {
-  return <h1> QuoteDetail</h1>
+  const params = useParams()
+  return (
+    <Fragment>
+      <h1> QuoteDetail</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
+  )
 }
 export default QuoteDetail
